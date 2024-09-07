274b896871608919dc9bc0f95269b04a6fe636f0
Aditya Das <adityadasofficial31@gmail.com>
2024-09-07 10:34:43 +0530

    reseller login and some others

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -12,15 +12,21 @@ import {
     REGISTER_RESELLER_REQUEST,
     REGISTER_RESELLER_SUCCESS,
     REGISTER_RESELLER_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
+    LOAD_RESELLER_REQUEST,
+    LOAD_RESELLER_SUCCESS,
+    LOAD_RESELLER_FAIL,
 } from '../constants/userConstants';
 
 
-export const userReducer = (state = { user: {} }, action) => {
+export const userReducer = (state = { user: {}, reseller: {} }, action) => {
     switch (action.type) {
         case CONSUMER_LOGIN_REQUEST:
         case REGISTER_CONSUMER_REQUEST:
         case RESELLER_LOGIN_REQUEST:
         case REGISTER_RESELLER_REQUEST:
+        case LOAD_RESELLER_REQUEST:
             return {
                 loading: true,
                 isAuthenticated: false,
@@ -28,16 +34,25 @@ export const userReducer = (state = { user: {} }, action) => {
         case CONSUMER_LOGIN_SUCCESS:
         case REGISTER_CONSUMER_SUCCESS:
         case RESELLER_LOGIN_SUCCESS:
+        case LOAD_RESELLER_SUCCESS:
             return {
+                ...state,
                 loading: false,
                 isAuthenticated: true,
                 user: action.payload,
+                reseller: action.payload,
             }
         case REGISTER_RESELLER_SUCCESS:
-            return{
+            return {
                 loading: false,
                 isAuthenticated: false,
             }
+        case LOGOUT_SUCCESS:
+            return {
+                loading: false,
+                user: null,
+                isAuthenticated: false,
+            };
         case CONSUMER_LOGIN_FAIL:
         case REGISTER_CONSUMER_FAIL:
         case RESELLER_LOGIN_FAIL:
@@ -47,6 +62,19 @@ export const userReducer = (state = { user: {} }, action) => {
                 isAuthenticated: false,
                 error: action.payload
             }
+        case LOAD_RESELLER_FAIL:
+            return {
+                loading: false,
+                isAuthenticated: false,
+                user: null,
+                error: action.payload,
+            };
+        case LOGOUT_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
         case CLEAR_ERRORS:
             return {
                 ...state,
